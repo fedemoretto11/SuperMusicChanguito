525e0ae18291367ed048d110eed36d8c74241c52
fedemoretto11 <fedemoretto94@gmail.com>
2023-06-28 01:23:10 -0300

    refactor(estilos): Actualizar estilos globales en el sitio
    - Ajustar tamaños de fuente y alturas de línea para una mejor legibilidad
    - Modificar la paleta de colores para mejorar la consistencia visual
    - Afinar los márgenes y rellenos para lograr un espaciado mejorado
    - Optimizar las consultas de medios para un diseño responsivo

--- js/productos.js
+++ js/productos.js
@@ -1,6 +1,6 @@
 // Funcion que obtiene la informacion de la API de MELI
 
-export async function obtenerProductos(categoria="MLA417638", limite="20") {
+export async function obtenerProductos(categoria="MLA417638", limite="5") {
   try {
     const result = await fetch(`https://api.mercadolibre.com/sites/MLA/search?category=${categoria}&limit=${limite}`);
     const productos = await result.json();
@@ -21,7 +21,7 @@ function card({id,imagen, altImagen, nombre,descripcion,cantidadDisponible, prec
       <p class="catalogo-cards_descripcion">${descripcion}</p>
       <p>Stock: ${cantidadDisponible}</p>
       <p>Precio: ${moneda} ${precio}</p>
-      <div class="center">
+      <div class="center catalogo-cards_buttons">
         <button class="agregarCarrito" value="${id}">Agregar al Carrito</button>
         <button class="agregarCantidad" value="${id}">+</button>
         <button class="disminuirCantidad" value="${id}">-</button>
